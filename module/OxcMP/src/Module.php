505e374b758c0bd85557af930ec7230b8bd5fd3f
SilviuG <killermosi@yahoo.com>
2017-02-09 17:49:37 +0200

    A slow start

--- module/OxcMP/src/Module.php
+++ module/OxcMP/src/Module.php
@@ -4,23 +4,37 @@ namespace OxcMP;
 
 use Zend\Mvc\MvcEvent;
 use Zend\Session\SessionManager;
+use Zend\Config\Config;
 
 class Module
 {
     const VERSION = '3.0.1';
 
+    /**
+     * Module config
+     * 
+     * @return string
+     */
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
     }
     
+    /**
+     * Module bootstrap
+     * 
+     * @param MvcEvent $event The event
+     * @return void
+     */
     public function onBootstrap(MvcEvent $event)
     {
-        $application = $event->getApplication();
-        $serviceManager = $application->getServiceManager();
+        $serviceManager = $event->getApplication()->getServiceManager();
+        $viewModel = $event->getApplication()->getMvcEvent()->getViewModel();
+    
+        $viewModel->config = new Config($serviceManager->get('Config'));
         
         // The following line instantiates the SessionManager and automatically
         // makes the SessionManager the 'default' one.
-        $sessionManager = $serviceManager->get(SessionManager::class);
+        $serviceManager->get(SessionManager::class);
     }
 }
