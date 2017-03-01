be0c0fda5ad5298ab131da7e5aba573ebab9d9de
SilviuG <killermosi@yahoo.com>
2017-03-01 14:08:58 +0200

    Added proper copyright headers, removed some extra files

--- db/create.sql
+++ db/create.sql
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2016-2017 OpenXcom Mod Portal Contributors
+ *
+ * This file is part of OpenXcom Mod Portal.
+ *
+ * OpenXcom Mod Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom Mod Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom Mod Portal. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 create table user (
     user_id int(10) not null auto_increment comment 'Internal identifier',
     is_orphan tinyint(1) not null default 0 comment 'If this user is orphan - it does not exist on the forum',
--- module/OxcMP/src/Module.php
+++ module/OxcMP/src/Module.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ * Copyright © 2016-2017 OpenXcom Mod Portal Contributors
+ *
+ * This file is part of OpenXcom Mod Portal.
+ *
+ * OpenXcom Mod Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom Mod Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom Mod Portal. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace OxcMP;
 
 use Zend\Mvc\MvcEvent;
@@ -102,8 +121,8 @@ class Module
     }
     
     /**
-     * Transform any value that that contains the dot character into a multi-dimensional array
-     * (each dot denotes another level)
+     * Transform any value that that contains the dot character into a multi-dimensional array,
+     * each dot denoting another level. Code adapted from: http://stackoverflow.com/a/9636021/1111983
      * 
      * @param array $data An associative array with the data
      * @return array
